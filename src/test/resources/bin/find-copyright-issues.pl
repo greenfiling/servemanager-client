b8d5c1b6f98a5b91ffc83b458f811ad3dbdc53fb
John Jetmore <john@greenfiling.com>
2023-01-28 17:47:46 -0500

    improvements to logic for finding missing/wrong copyright year
    * general restructuring to make it more obvious what is happening
    * add new test that notices when uncommitted file have a non-current-year copyright year

--- src/test/resources/bin/find-copyright-issues.pl
+++ src/test/resources/bin/find-copyright-issues.pl
@@ -29,9 +29,13 @@ exit;
 sub wanted {
 	return 0 if ($File::Find::name !~ /\.(java|pl)/);
 	return 0 if ($File::Find::dir =~ m|$findsrc/target|);
+	my $currentYear = (localtime())[5] + 1900;
 
 	my $relativeFile = $File::Find::name;
 	$relativeFile =~ s|$findsrc/||;
+	# next if ($relativeFile ne 'test/resources/bin/find-copyright-issues.pl');
+	# next if ($relativeFile !~ m|JobClient_Unit|);
+	# print STDERR "considering $relativeFile\n";
 
 	# we can't enforce this because some files might not be in version control yet
 	# if (!exists($fileToYearMap->{$relativeFile})) {
@@ -45,16 +49,42 @@ sub wanted {
 		$header .= <I>;
 	}
 	close(I);
-	if ($header !~ / Copyright \d+(-\d+)? Green Filing, LLC/ || $header !~ /Licensed under the Apache License/) {
-		print STDERR "missing/corrupt copyright in $relativeFile\n";
+
+	my $fileYear = '';
+	my $licensed = 0;
+	my $copyrighted = 0;
+	if ($header =~ / Copyright (?:\d+-)?(\d\d\d\d) Green Filing, LLC/) {
+		$copyrighted = 1;
+		$fileYear = $1;
+	}
+	if ($header =~ /Licensed under the Apache License/) {
+		$licensed = 1;
 	}
 
-	if (exists($fileToYearMap->{$relativeFile}) && $header =~ / Copyright (?:\d+-)?(\d\d\d\d) Green Filing, LLC/) {
-		my $fileYear = $1;
-		if ($fileYear ne $fileToYearMap->{$relativeFile}) {
-			print STDERR "last copyright year not accurate (file: $fileYear, vcs: $fileToYearMap->{$relativeFile}) in $relativeFile\n";
+	if (!$licensed || !$copyrighted) {
+		error("missing/corrupt copyright in $relativeFile (lic: $licensed, cr: $copyrighted)");
+	}
+	else {
+		# $fileYear has to be set to a 4 digit number in order to be in this block
+
+		if (exists($fileToYearMap->{$relativeFile})) {
+			if ($fileYear ne $fileToYearMap->{$relativeFile}) {
+				error("copyright end year not accurate in $relativeFile (file: $fileYear, vcs: $fileToYearMap->{$relativeFile})");
+			}
+		}
+		else {
+			# if it doesn't exist in fileToYearMap that means it's not committed to git yet, meaning this is a new file.  In that case
+			# we check to make sure the copyright year is the current year
+			if ($fileYear != $currentYear) {
+				error("copyright end year not current year for uncommitted file in $relativeFile (file: $fileYear, current: $currentYear)");
+			}
 		}
 	}
+}
+
+sub error {
+	my $msg = shift;
+	print STDERR "$msg\n";
 }
 
 # pieced together from multiple answers from https://serverfault.com/questions/401437/how-to-retrieve-the-last-modification-date-of-all-files-in-a-git-repository
