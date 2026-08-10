61abf8773e038ae76a1ecfc51a44468af245d2f5
anaverul <ana.verulidze@gmail.com>
2026-08-10 13:56:30 -0400

    RM-8986 add AgencyFilter class

--- src/main/java/com/greenfiling/smclient/model/exchange/AgencyFilter.java
+++ src/main/java/com/greenfiling/smclient/model/exchange/AgencyFilter.java
@@ -1,7 +1,110 @@
 package com.greenfiling.smclient.model.exchange;
 
+import java.util.ArrayList;
+
 import com.greenfiling.smclient.model.internal.FilterBase;
 
 public class AgencyFilter extends FilterBase {
 
+  private String zipcode;
+  private String address;
+  private String email;
+  private String type;
+  private Integer[] accountIds;
+  private Boolean usingSuggestedRetailPricing;
+  private Integer limit;
+
+  public Integer[] getAccountIds() {
+    return accountIds;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  @Override
+  public ArrayList getFilters() {
+    ArrayList<FilterPair> pairs = super.getFilters();
+
+    if (getZipcode() != null) {
+      pairs.add(new FilterPair("zipcode", getZipcode()));
+    }
+
+    if (getAddress() != null) {
+      pairs.add(new FilterPair("address", getAddress()));
+    }
+
+    if (getEmail() != null) {
+      pairs.add(new FilterPair("email", getEmail()));
+    }
+
+    if (getType() != null) {
+      pairs.add(new FilterPair("type", getType()));
+    }
+
+    if (getAccountIds() != null) {
+      for (Integer accountId : getAccountIds()) {
+        pairs.add(new FilterPair("account_ids[]", accountId.toString()));
+      }
+    }
+
+    if (getUsingSuggestedRetailPricing() != null) {
+      pairs.add(new FilterPair("using_suggested_retail_pricing", getUsingSuggestedRetailPricing().toString()));
+    }
+
+    if (getLimit() != null) {
+      pairs.add(new FilterPair("limit", getLimit().toString()));
+    }
+
+    return pairs;
+  }
+
+  public Integer getLimit() {
+    return limit;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public Boolean getUsingSuggestedRetailPricing() {
+    return usingSuggestedRetailPricing;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setAccountIds(Integer[] accountIds) {
+    this.accountIds = accountIds;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setLimit(Integer limit) {
+    this.limit = limit;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public void setUsingSuggestedRetailPricing(Boolean usingSuggestedRetailPricing) {
+    this.usingSuggestedRetailPricing = usingSuggestedRetailPricing;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
 }
